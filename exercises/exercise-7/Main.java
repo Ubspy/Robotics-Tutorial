421ec74c9d5477c88ac5819a8ed80bda79866965
Ubspy <jack-m@live.com>
2021-01-20 11:42:19 -0600

    Added base files for ex-7

--- exercises/exercise-7/Main.java
+++ exercises/exercise-7/Main.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
-
 class Main
 {
-  public static void main(String[] args)
-  {
-    
-  }
+    public static void main(String[] args)
+    {
+        ShapeController shapeController = new ShapeController();
+		    shapeController.run();
+    }
 }
